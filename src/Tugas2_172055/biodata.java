993468d5c844534be97255461fbaf01e98fe49ea
tenriajeng <ilhamtenriajeng03@gmail.com>
2019-10-01 03:58:43 +0800

    sourcecode

--- src/Tugas2_172055/biodata.java
+++ src/Tugas2_172055/biodata.java
@@ -1,9 +1,24 @@
 package Tugas2_172055;
+import java.util.Scanner;
 
 public class biodata 
 {
-    public static void main(String[] args) 
+     public static void main(String[] args) 
     {
-        System.err.println("Hellow world");
+        String nama, nim, umur;
+        
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("BIODATA MAHASISWA\n");
+        
+        System.out.print("Masukkan Nama Anda \t: ");
+        nama = input.nextLine();
+        System.out.print("Masukkan Stambuk Anda \t: ");
+        nim = input.nextLine();
+        System.out.print("Masukkan Umur Anda \t: ");
+        umur = input.nextLine();
+        
+        System.out.println("");
+        System.out.println("Nim " + nim + " Dengan Nama " + nama + " Berumur " + umur + " Tahun");        
     }
 }
