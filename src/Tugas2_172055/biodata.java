e83bfa90e56b409528b3ac9ba54e01228b320cea
tenriajeng <ilhamtenriajeng03@gmail.com>
2019-10-01 03:39:47 +0800

    hellow world

--- src/Tugas2_172055/biodata.java
+++ src/Tugas2_172055/biodata.java
@@ -1,14 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Tugas2_172055;
 
-/**
- *
- * @author thisismusni
- */
-public class biodata {
-    
+public class biodata 
+{
+    public static void main(String[] args) 
+    {
+        System.err.println("Hellow world");
+    }
 }
