06f9532e9ae77f3ef81f23dba24bb9ab64b36cb0
thisismusni <thisismusni@10.175.103.151>
2019-10-01 21:23:06 +0800

    fix

--- src/Tugas2_172055/biodata.java
+++ src/Tugas2_172055/biodata.java
@@ -5,20 +5,20 @@ public class biodata
 {
      public static void main(String[] args) 
     {
-        String nama, nim, umur;
+        String name, stb, umur;
         
-        Scanner input = new Scanner(System.in);
+        Scanner baca = new Scanner(System.in);
         
         System.out.println("BIODATA MAHASISWA\n");
         
         System.out.print("Masukkan Nama Anda \t: ");
-        nama = input.nextLine();
+        name = baca.nextLine();
         System.out.print("Masukkan Stambuk Anda \t: ");
-        nim = input.nextLine();
+        stb = baca.nextLine();
         System.out.print("Masukkan Umur Anda \t: ");
-        umur = input.nextLine();
+        umur = baca.nextLine();
         
         System.out.println("");
-        System.out.println("Nim " + nim + " Dengan Nama " + nama + " Berumur " + umur + " Tahun");        
+        System.out.println("Nim " + stb + " Dengan Nama " + name + " Berumur " + umur + " Tahun");        
     }
 }
